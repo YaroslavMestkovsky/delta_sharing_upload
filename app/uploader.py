d9f6a2a771b3c517a7a30e4b28f043ae915a7fe4
Yaroslav Mestkovsky <mestkovskij@obt-vlg.ru>
2025-02-12 17:40:22 +0300

    # 9. Забытые костыли.

--- app/uploader.py
+++ app/uploader.py
@@ -13,7 +13,7 @@ class BaseUploader:
 
     def __init__(self, *args, **kwargs):
         logging.basicConfig(
-            # filename='../upload.log', todo
+            filename='../upload.log',
             level=logging.INFO,
             format='%(levelname)s - %(message)s'
         )
